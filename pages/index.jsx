47561702f302b8729d0a258d317ef0738c777e03
dora <dora80823@gmail.com>
2020-05-17 02:54:55 +0800

    feature/增加資料夾&redux

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,7 @@
+import React from 'react';
+import { connect } from 'react-redux';
 import Link from 'next/link';
+import { loadData } from '../store/actions';
 
 const Index = () => {
     return (
@@ -10,4 +13,14 @@ const Index = () => {
         </>
     );
 };
-export default Index;
+
+Index.getInitialProps = (props) => {
+    const { store, isServer } = props.ctx;
+    // console.log(store.getState().defultData);
+    // if (!store.getState().defultData) {
+    store.dispatch(loadData());
+    // }
+    return { isServer };
+};
+
+export default connect()(Index);
